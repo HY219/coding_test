a6460a021b69f7311163c4b9399bbbc93016b1ba
hy219 <jhy_0219@daum.net>
2023-03-21 23:59:21 +0900

    문제21: set은 어떻게 만드나요?

---
+++ 0321-JS100-Q21.js
@@ -0,0 +1,24 @@
+//set은 어떻게 만드나요?
+//다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+/**
+ * 1. var x = {1,2,3,5,6,7};
+ * 2. var x = {};
+ * 3. var x = new Set('javascript');
+ * 4. var x = new Set(range(5));
+ * 5. var x = new Set();
+ */
+
+//Set 객체
+//자료형에 관계 없이 원시 값과 객체 참조 모두 유일한 값을 저장할 수 있다.
+//Set() : Create a new Set object
+//Set.prototype.add(value)
+//Set.prototype.clear()
+//Set.prototype.delete(value)
+//Set.prototype.has(value)
+
+//var mySet = new Set();
+//mySet.add(1); //Set { 1 }
+//mySet.add(5); //Set {1, 5}
+//mySet.add(5); //Set {1, 5}
+
+//3번, 5번
