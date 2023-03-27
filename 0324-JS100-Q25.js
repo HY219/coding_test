96cb21bc534e0f40efa485bf7dd54f1121b78761
hy219 <jhy_0219@daum.net>
2023-03-27 16:28:41 +0900

    함수 만들기

---
+++ 0324-JS100-Q25.js
@@ -0,0 +1,12 @@
+//원의 넓이를 구하세요
+//r*r*3.14
+const n = prompt("반지름의 길이를 입력하세요");
+// const circle = n * n * 3.14;
+// console.log(circle);
+
+function circle(n) {
+  n * n * 3.14;
+}
+circle();
+
+//함수 만들기
